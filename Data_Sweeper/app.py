17ea7f2dfa1fd13bfca528d63ec84f5570151db4
OsamabinAdnan <binadnanosama@gmail.com>
2025-02-24 17:56:06 +0500

    code updated

--- Data_Sweeper/app.py
+++ Data_Sweeper/app.py
@@ -5,7 +5,7 @@ from io import BytesIO
 
 # Set up our app
 st.set_page_config(page_title="📀 Data Sweeper", layout="wide")
-st.title("📀_Data Sweeper")
+st.title("📀 Data Sweeper")
 st.write("Transform your files 📂 between CSV and Excel formats")
 
 # File upload
